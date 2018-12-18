767b310f7c13cc2210c2edf37690afd546febb16
adirizka7 <adiunity@gmail.com>
2018-12-19 02:18:41 +0700

    major edit

--- mpplproject/resources/views/layouts/app.blade.php
+++ mpplproject/resources/views/layouts/app.blade.php
@@ -297,7 +297,7 @@
   </script>
 
   <!-- JQuery -->
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+  <!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script> -->
   <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
 
   <!-- ChartJS -->
